0a241a44f984bf6cb7acee5e72998fa8b81ef6e2
Marcin Weglowski <marcin.weglowski1@gmail.com>
2024-06-05 14:00:04 +0200

    f

--- src/components/pages/realestates/RealEstatesPage.jsx
+++ src/components/pages/realestates/RealEstatesPage.jsx
@@ -12,7 +12,7 @@ const RealEstatesPage = () => {
       try {
         const response = await fetch("https://webscraper-api.vercel.app/");
 
-        // const response = await fetch("http://localhost:5000");
+        // const response = await fetch("http://localhost:5000/");
 
         if (!response.ok) {
           throw new Error(`Error: ${response.status}`);
